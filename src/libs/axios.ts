e0ab0386508965db9683cc154ae6df499cbcdbb8
gitkazumaa <kazumaalternate@gmail.com>
2023-12-31 13:32:17 -0800

    Update

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,3 +1,6 @@
 import Axios from "axios"
 
-export const axios = Axios.create()
+export const axios = Axios.create({
+	baseURL: "https://aim-blitz-server.onrender.com",
+	withCredentials: true,
+})
