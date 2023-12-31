8134a812503fbab4c7837ee957f02a2259cbc4ac
gitkazumaa <kazumaalternate@gmail.com>
2023-12-31 15:21:42 -0800

    Update

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,6 +1,3 @@
 import Axios from "axios"
 
-export const axios = Axios.create({
-	baseURL: "https://aim-blitz-server.onrender.com",
-	withCredentials: true,
-})
+export const axios = Axios.create()
--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,11 @@ export default defineConfig({
 			"@": resolve(__dirname, "./src"),
 		},
 	},
+	server: {
+		proxy: {
+			"/api": {
+				target: "https://aim-blitz-server.onrender.com/api",
+			},
+		},
+	},
 })
